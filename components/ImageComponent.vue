bd17d332a35ae4fc6de644f17369d5c32da63d6e
Pablo Bacchetta <polbac@gmail.com>
2020-02-24 13:45:11 -0300

    remove dep

--- components/ImageComponent.vue
+++ components/ImageComponent.vue
@@ -1,16 +1,12 @@
 <template>
-    
     <div>
         <img v-bind:src='source.url' v-bind:alt='source.alt' />
     </div>
 </template>
 
 <script>
-import { Parraph } from './Parraph';
-
 export default {
     name: 'ImageComponent',
-    component: [ Parraph ],
     props: {
         source: {
             type: Object,
